56e4ffef2cf66fe77fa3b83200554af02daae75e
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2019-02-05 08:32:51 +0000

    begin sorting eslint out to run from the npm script.

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var server = require("./server/server");
 var port = process.env.PORT || 8080;
 
 server(port);
+// eslint-disable-next-line no-console
 console.log("Server running on port " + port);
