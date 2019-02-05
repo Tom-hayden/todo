73b0c5d3564a04a40facea6eba07665afdc9f87d
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2019-02-05 09:28:14 +0000

    Remove gulp

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,7 @@
-var server = require("./server/server");
+var server = require("../server/server");
 
 var port = process.env.PORT || 8080;
 
 server(port);
-// eslint-disable-next-line no-console
+
 console.log("Server running on port " + port);
