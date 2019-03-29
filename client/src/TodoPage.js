a0987a485f3005b104bf0d3f0f465f269b151ff4
Thomas Hayden <thayden@scottlogic.com>
2019-03-29 10:22:08 +0000

    Pullout another component from TodoPage

--- client/src/TodoPage.js
+++ client/src/TodoPage.js
@@ -2,11 +2,9 @@ import React, { Component } from "react";
 import "./TodoPage.css";
 import socketIOClient from "socket.io-client";
 import TodoHeader from "./TodoHeader";
-import DeleteAllCompletedButton from "./DeleteAllCompletedButton";
-import TodoList from "./TodoList";
 import TodoSubmit from "./TodoSubmit";
-import LoadingPage from "./LoadingPage";
 import todoFilters from "./todoListFilters";
+import TodoListContainer from "./TodoListContainer";
 
 
 class TodoPage extends Component {
@@ -42,27 +40,11 @@ class TodoPage extends Component {
 
     render() {
 
-        let todoList;
-        if (this.state.hasRecievedData) {
-            todoList = (
-                <div id="todo-list-container">
-                    <TodoList todos={this.state.todos} socket={this.socket} filter={this.state.filter.bind(this)}/>
-                    <DeleteAllCompletedButton todos={this.state.todos} socket={this.socket} />
-                </div>
-            );
-        } else {
-            todoList = (
-                <div>
-                    <LoadingPage />
-                </div>
-            )
-        }
-
         return (
             <div className="TodoPage">
                 <TodoHeader todos={this.state.todos} onFilterChange={this.onFilterChange.bind(this)}/>
                 <TodoSubmit socket={this.socket}/>
-                {todoList}
+                <TodoListContainer todos={this.state.todos} socket={this.socket} filter={this.state.filter.bind(this)}/>
             </div>
         )
     }
