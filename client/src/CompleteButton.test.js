d02b01bffac2109219cb298f4a3e8297a45e8ad1
Thomas Hayden <thayden@scottlogic.com>
2019-04-02 12:06:33 +0100

    Add test for CompleteButton component

---
+++ client/src/CompleteButton.test.js
@@ -0,0 +1,15 @@
+import CompleteButton from "./CompleteButton";
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+    const todoID = "3";
+    const todo = {
+        id: todoID
+    };
+    const socket = {};
+    const component = renderer.create(
+        CompleteButton({todo, socket}) 
+    ).toJSON();
+
+    expect(component).toMatchSnapshot();
+});
