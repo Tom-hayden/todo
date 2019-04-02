9801c263fcf6e5b211ef1e9afa849b4366451377
Thomas Hayden <thayden@scottlogic.com>
2019-04-02 11:10:21 +0100

    Add completeTodo unit test

---
+++ client/src/buttonFunctions.test.js
@@ -0,0 +1,16 @@
+import {completeTodo} from "./buttonFunctions.js";
+
+
+it("Calls emit on socket", () =>{
+    let socket = {};
+    let todo = {};
+    const todoID = "3"
+
+    socket["emit"] = jest.fn();
+    todo.id = todoID;
+
+    completeTodo(socket,todo);
+
+    expect(socket.emit).toHaveBeenCalledWith("completeTodo",todoID);
+})
+
